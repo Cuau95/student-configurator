fe9e3c4b17d98a0f08eb9187f1613976293a6cc5
Cuau95 <elzua_ecm@live.com.mc>
2019-05-22 19:46:44 -0500

    add password attribute

--- src/main/java/com/titulation/student/configurator/domain/model/Student.java
+++ src/main/java/com/titulation/student/configurator/domain/model/Student.java
@@ -28,6 +28,15 @@ public class Student implements Serializable {
     @ManyToOne(cascade=CascadeType.ALL)
     @JoinColumn(name="id_carrera")
     private Career carrera;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String getId() {
         return idAlumno;
